c0ee35030858129b6a3db1e643e0ebba75f544f8
Nitheshnithu18 <nithesh09052001@gmail.com>
2024-06-24 10:44:10 +0530

    Add files via upload

---
+++ ConsoleApp1/AccessString/AccessString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Rahul";
+            Console.WriteLine(myName[0]);
+            Console.WriteLine(myName[1]);
+            Console.WriteLine(myName[2]);
+            Console.WriteLine(myName[3]);
+            Console.WriteLine(myName[4]);
+
+            Console.ReadLine();
+        }
+    }
+}
