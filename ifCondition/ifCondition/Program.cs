4398c6564e9e29d30a67f3493ddd592746276f67
Nitheshnithu18 <nithesh09052001@gmail.com>
2024-06-24 10:40:37 +0530

    Add files via upload

---
+++ ifCondition/ifCondition/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifCondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b = 15.5;
+            if(a > b)
+            {
+                Console.WriteLine("A is greater than B");
+            }
+            else
+            {
+                Console.WriteLine("Ais smaller than B");
+                Console.ReadLine();
+            }
+        }
+    }
+}
